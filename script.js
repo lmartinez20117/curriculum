0f5e7647f687761bfb2f6f2ff3378e171904f4ff
lmartinez20117 <lmartinez20117@iesjoanramis.org>
2025-10-03 08:53:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,26 @@ function canvifoto(){
     document.getElementById("foto").src = foto
 }
 function harryPotter(){
-    if(document.getElementeryById("foto").hidden == true){
-        document.getElementeryById("foto").hidden = false
-    }else{
-        document.getElementeryById("foto").hidden = true
-    }
+    document.getElementById("foto").style ="display:none"
+       
+}
+function canviFons(){
+    let body = document.getElementsByTagName("body")[0]
+    let foto = prompt("DONDE QUIERES VIAJAR?")
+    body.style = "background-image: url('"+foto+"'); background-size: cover;" ;
+}
+function tradueix(){
+    let body = document.getElementsByTagName("body")[0];
+    body.style = "font-family: Manrope, sans-serif;"
+
+}
+function afegirInfo(){
+    let llista = document.getElementById("expL");
+    let element = document.createElement("li");
+    element.innerHTML = prompt("QUE QUIERES AÑADIR?")
+    llista.appendChildId(element)
+}
+function hacerfotogrande(){
+    let foto = document.getElemenyById("foto")
+    width= "3000"
 }
